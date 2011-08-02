dfeaba863dc7a37656cfa1d3a465f20b43205fb1
Rory Walker <redrory@gmail.com>
2011-08-02 06:41:10 -0500

    Initial TG commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trendygits::Application.config.secret_token = '6e5e03d6641680f57f81f2677ca2e284090d633f489d7f69793adf751cf773ebde305ac5cc0650089f0dc62bc05245a8b79824dfa58bd4bc79ca5d74675de8c6'
